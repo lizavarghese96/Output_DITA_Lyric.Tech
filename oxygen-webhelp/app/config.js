98ae0b51d84cea9d8dff2a69315405db490c3702
Glenn Liza Varghese <glennliza55231196@gmail.com>
2026-04-23 22:30:12 +0530

    Update

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20260423220424';
+                args = '20260423222942';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
